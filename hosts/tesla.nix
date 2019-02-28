2dc4e4fba8850d25be0418215f9d67aa6920eefa
Florian Franzen <Florian.Franzen@gmail.com>
2019-02-28 23:03:44 +0100

    hosts: Add tesla

---
+++ hosts/tesla.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable ZFS support
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.enableUnstable = true;
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+
+  # Network setup
+  networking = {
+    hostName = "tesla";
+    hostId = "bae9037f";
+  }; 
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    # Enable sensord deamon build
+    lm_sensors = pkgs.lm_sensors.override { sensord = true; }; 
+  };
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    lm_sensors smartmontools
+  ];
+
+  # Disable power button
+  services.logind.extraConfig = "HandlePowerKey=suspend";
+
+  # Cool HDD array based on SMART temp
+  services.thinkfan = {
+    enable = true;
+    smartSupport = true;
+    fan = "pwm_fan /sys/class/hwmon/hwmon0/device/pwm2";
+    sensors = ''
+      atasmart /dev/sdb
+      atasmart /dev/sdc
+      atasmart /dev/sdd
+      atasmart /dev/sde
+      atasmart /dev/sdf
+    '';
+    levels = ''
+      (  0,   0,  28)
+      ( 90,  25,  33)
+      (110,  30,  38) 
+      (130,  35,  43)
+      (150,  40,  48)
+      (170,  45,  53)
+    '';
+    # Slower curve:
+    #  ( 90,  35,  43)
+    #  (110,  40,  48)
+    #  (150,  45,  53)
+    #  (190,  50, 100) 
+  };
+}
--- profiles/minimal.nix
+++ profiles/minimal.nix
@@ -17,6 +17,7 @@
 
     bind
      
+    vim
     file
     git 
     gnupg
@@ -31,6 +32,8 @@
       keyMode = "vi";
       clock24 = true;
     };
+
+    mtr.enable = true;
   
     zsh = { 
       enable = true;
@@ -44,10 +47,13 @@
   security.pam.enableSSHAgentAuth = true;
 
   # Add default users
-  users.extraUsers.florian = {
+  users.users.florian = {
     isNormalUser = true;
     description = "Florian Franzen";
     extraGroups = [ "wheel" ];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCXZ+XdLV1D5NqKCqY/PU2ntCm1v7zwtBXEK6px12E04d4lS6fjnbJ+bEaw1GtHI/KMhz+rmFRVVGd/gjONtGJ6BCxHelHcuw0/sCvuuwYk8WkdSRD7nZ5CEJt+xiyqcUC4uzCd9woR/+i7vfNTabPvx1+fnlCS/+HmpViv7UiaOXxzsqHPYCOXWl3I+EiI0UGOGJoZFAnfb+7yNX0SnzCZsLZJ5s2jr5V1NqEql70d3JXQDY4uQSsbMjdsc0jopc1ICcDqK9td/WdV4votSggY0Mz6oLw3vcdp+kyIreoGiGF+1yMIc03xIRPDzrA/Y2rascImLpBBV3zZQ6rSOYPhufWs74aueVZTiv4MMIxb4R+yZs93BHEtd76wQTE+VnxXTkvA8Lb2doh3WrcdX+2tiDWUXfjNg0Ko0QNH8liY7KuoeP4hgx2iSWpHoGOhYP1006es/TgFtWo21ZHmGiuPC/mWwUjLFddbP+ERqIaGtuE+62dJZTK3inD3xQqtSivTW1ZOcViZWLhM5mFYj6Hlklboi6HbDydLi9nTat+yfF+5KOiH69HwPjtTIrfTUIndQqTZSfRi/MLPZRfxLo3UeqSPWnbJe7A56Mc3kvlQbjrNkcJTwMIeIiVbDTwtoXiMwk8Z/r/R7+9Z1r2/6uzBLPaiNNdxZ/E6EKl7ThaAHQ== cardno:000604936884" 
+    ];
   };
 }
