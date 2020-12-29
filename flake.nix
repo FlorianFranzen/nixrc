74a1505ce6b1ff59cb9ca264e7120c29fcc8ec80
Florian Franzen <Florian.Franzen@gmail.com>
2020-12-29 01:01:01 +0100

    flake: check chomsky build

--- flake.nix
+++ flake.nix
@@ -2,12 +2,20 @@
   description = "Nix and NixOS configurations of Florian Franzen";
 
   inputs = {
-     nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+    # Basis for configuration
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
+
+    # Useful overlay
+#    w3fpkgs.url = "git+https://gitlab.w3f.tech/florian/w3fpkgs";
+#    w3fpkgs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs }: {
 
     nixosConfigurations = import ./hosts { pkgs = nixpkgs; };
 
+    checks.x86_64-linux = {
+      chomsky = self.nixosConfigurations.chomsky.config.system.build.toplevel;
+    };
   };
 }
