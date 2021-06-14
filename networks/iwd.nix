79a942cd5cc56038636c2e9da4996ac8773c6eb7
Florian Franzen <Florian.Franzen@gmail.com>
2021-06-14 15:26:40 +0200

    iwd: rename interfaces again

--- networks/iwd.nix
+++ networks/iwd.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 { 
   # Enable dynamic configured iwd
   networking.wireless.iwd.enable = true;
+
+  # Reenable renaming interfaces
+  systemd.network.links."80-iwd" = lib.mkForce {};
 }
 
 
