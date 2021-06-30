04d2721bda96da00c16fc4b084258db203bc8e98
Florian Franzen <Florian.Franzen@gmail.com>
2021-06-30 10:42:29 +0200

    hydra: enable automatic gc

--- services/hydra.nix
+++ services/hydra.nix
@@ -1,10 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  nix.extraOptions = ''
-    allowed-uris = https://github.com/ https://static.rust-lang.org/dist/
-  '';
+  nix = {
+    # Garbage collect nix store once a week
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
 
+    # Allow access to needed external ressources
+    extraOptions = ''
+      allowed-uris = https://github.com/ https://static.rust-lang.org/dist/
+    '';
+  };
+
+  # Enable hydra with local builds
   services.hydra = {
     enable = true;
     hydraURL = "http://localhost:3000";
@@ -13,9 +23,10 @@
     useSubstitutes = true;
     minimumDiskFree = 50;	
   };
-  
+
   networking.firewall.allowedTCPPorts = [ 3000 ];
 
+  # Useful tools
   environment.systemPackages = [
     pkgs.nix-top
     pkgs.hydra-cli
