f0144b42b8c211ef0530763406897391a246ffaa
Florian Franzen <Florian.Franzen@gmail.com>
2021-06-30 09:19:00 +0200

    services: add steam

---
+++ services/steam.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+ 
+  # Enable kodi media server
+  services.xserver = {
+    enable = true;
+
+    libinput.enable = true;
+
+    displayManager.auto = {
+      enable = true;
+      user = "steam";
+    };
+    
+    desktopManager.steam.enable = true;
+    desktopManager.steam.package = pkgs.steamos-compositor-plus;
+  };
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # 32bit video and audio support
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  users.users.steam = {
+    isSystemUser = true;
+    home = "/data/steam";
+  };
+}
