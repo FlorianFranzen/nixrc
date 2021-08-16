95912cb52db61e3ac65565e1bb6dcb9b0aaf38fc
Florian Franzen <Florian.Franzen@gmail.com>
2021-08-16 14:20:43 +0200

    lightdm: add touchpad support

--- desktops/lightdm.nix
+++ desktops/lightdm.nix
@@ -3,7 +3,16 @@
 {
   # Enable LightDM display manager
   services.xserver = {
-    enable = true; # TODO: Switch to non-X11 greeter
+    # TODO: Switchs to non-X11 greeter
+    enable = true;
+
+    # Add trackpad support
+    libinput = {
+      enable = true;
+      touchpad.tapping = false;
+    };
+
+    # Enable greeter itself
     displayManager.lightdm.enable = true;
   };
 }
