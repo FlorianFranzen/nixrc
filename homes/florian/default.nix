e438fba7ed7c6effcfd1f2a65a3f718284bb4165
Florian Franzen <Florian.Franzen@gmail.com>
2021-08-17 10:12:19 +0200

    florian: add basic zsh config

--- homes/florian/default.nix
+++ homes/florian/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./zsh.nix
     ./sway.nix
     ./spacemacs.nix
   ];
---
+++ homes/florian/zsh.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    #enableSyntaxHighlighting = true;
+
+    defaultKeymap = "viins";
+
+    localVariables = {
+      # Use default powerlevel config for now
+      POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD = true;
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        src = pkgs.zsh-powerlevel10k;
+      }
+    ];
+
+    shellAliases = {
+      cat = "bat --plain";
+    };
+  };
+}
