bbaf578acc8ab0a7cb130dc1f5b88d317cb61033
Florian Franzen <Florian.Franzen@gmail.com>
2021-09-15 14:49:07 +0200

    florian: add sway background

--- homes/florian/themes.nix
+++ homes/florian/themes.nix
@@ -1,6 +1,11 @@
-{ pkgs, ...}:
+{ srcs, pkgs, ...}:
 
 {
+  # Set desktop background
+  wayland.windowManager.sway.config.output."*" = {
+    background = "${srcs.self}/assets/nixish-dark.png fill";
+  };
+
   # Set gtk look and feel
   gtk = {
     enable = true;
