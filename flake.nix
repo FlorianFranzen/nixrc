ae89a202cee25f7e0bc819fb8219bafbfcff4c8a
Florian Franzen <Florian.Franzen@gmail.com>
2021-10-23 10:34:51 +0200

    flake: cleanup inputs

--- flake.nix
+++ flake.nix
@@ -4,17 +4,13 @@
   inputs = {
     # Flake helpers
     digga.url = "github:divnix/digga";
-    digga.inputs.nixpkgs.follows = "nixos";
-    digga.inputs.nixlib.follows = "nixos";
+    digga.inputs.nixpkgs.follows = "nixpkgs";
+    digga.inputs.nixlib.follows = "nixpkgs";
     digga.inputs.latest.follows = "latest";
     digga.inputs.home-manager.follows = "home";
 
-    bud.url = "github:divnix/bud";
-    bud.inputs.nixpkgs.follows = "nixos";
-    bud.inputs.devshell.follows = "digga/devshell";
-
     # Host configurations
-    nixos.url = "github:florianfranzen/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:florianfranzen/nixpkgs/nixos-21.05";
 
     # Latest package set
     latest.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -24,10 +20,11 @@
 
     # Home management
     home.url = "github:nix-community/home-manager/release-21.05";
-    home.inputs.nixpkgs.follows = "nixos";
+    home.inputs.nixpkgs.follows = "nixpkgs";
 
     # Firefox Addons
     firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+    firefox-addons.inputs.nixpkgs.follows = "nixpkgs";
 
     # Latest emacs and framework
     emacs-overlay.url = "github:nix-community/emacs-overlay";
@@ -39,8 +36,7 @@
   outputs = {
     self,
     digga,
-    bud,
-    nixos,
+    nixpkgs,
     latest,
     hardware,
     home,
@@ -61,7 +57,7 @@
       (importTree dir);
 
     # Turn string list of subdirs into attrset of tree imports
-    mkImportables = dirs: nixos.lib.genAttrs dirs (n: importTree (./. + "/${n}"));
+    mkImportables = dirs: nixpkgs.lib.genAttrs dirs (n: importTree (./. + "/${n}"));
 
     # Shared host and home modules 
     modules = importModuleTree ./modules;
@@ -83,7 +79,7 @@
     supportedSystems = ["aarch64-linux" "x86_64-linux"];
 
     # nixpkgs versions and configs
-    channels.nixos = {
+    channels.nixpkgs = {
       # TODO Check sharedOverlays
       overlays = [ 
         emacs-overlay.overlay 
@@ -97,14 +93,13 @@
       hostDefaults = {
         system = "x86_64-linux";
 
-        channelName = "nixos";
+        channelName = "nixpkgs";
 
         modules = [
           modules.hosts
           #digga.nixosModules.bootstrapIso
           #digga.nixosModules.nixConfig
           home.nixosModules.home-manager
-          bud.nixosModules.bud
         ];
       };
 
