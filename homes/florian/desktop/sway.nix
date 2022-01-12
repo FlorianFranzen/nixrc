a9ed2f5db50a7e4643775862ffdf9503f610fb0d
Florian Franzen <Florian.Franzen@gmail.com>
2022-01-12 11:14:15 +0100

    sway: move more to user config

--- homes/florian/desktop/sway.nix
+++ homes/florian/desktop/sway.nix
@@ -5,19 +5,59 @@ let
 
   gap = 10;
 in {
-  programs.alacritty.enable = true;
 
   wayland.windowManager.sway = {
     enable = true;
 
-    # Use system's sway and Xwayland
-    package = null;
+    wrapperFeatures.gtk = true;
+
+    extraOptions = [ "--my-next-gpu-wont-be-nvidia" ];
+
+    extraSessionCommands = let
+      schema = pkgs.gsettings-desktop-schemas;
+      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+    in ''
+      # Enable libappindicator support
+      export XDG_CURRENT_DESKTOP=sway
+
+      # Enable wayland backends
+      export XDG_SESSION_TYPE=wayland
+
+      # Use CLUTTER wayland backend
+      export CLUTTER_BACKEND=wayland
+
+      # Enable mozilla wayland backend
+      export MOZ_ENABLE_WAYLAND=1
+
+      # Enable smooth scrolling
+      export MOZ_USE_XINPUT2=1
+
+      # Enable mozilla dbus
+      export MOZ_DBUS_REMOTE=1
+
+      # Enable LibreOffice gtk3 backend
+      export SAL_USE_VCLPLUGIN=gtk3
+
+      # Configure Qt wayland backend
+      export QT_WAYLAND_FORCE_DPI=physical
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+      # Fix Java AWT applications
+      export _JAVA_AWT_WM_NONREPARENTING=1
+
+      # Use GTK theme and integration
+      export DESKTOP_SESSION=gnome
+      export QT_QPA_PLATFORMTHEME=gtk2
+
+      # Fix gsettings
+      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+    '';
 
     config = {
       # Default input config
       input."*" = {
         xkb_layout = "eu";
-        xkb_options = "compose:ralt,terminate:ctrl_alt_bksp";
+        xkb_options = "compose:ralt";
         xkb_numlock = "enabled";
 
         natural_scroll = "enabled";
--- profiles/desktops/sway.nix
+++ profiles/desktops/sway.nix
@@ -28,50 +28,6 @@
   # Install sway
   programs.sway = {
     enable = true;
-    extraOptions = [ "--my-next-gpu-wont-be-nvidia" ];
-    extraSessionCommands = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-    in ''
-      # Set default keyboard layout (still needed?)
-      export XKB_DEFAULT_LAYOUT=eu
-      export XKB_DEFAULT_OPTIONS=compose:ralt
-
-      # Enable libappindicator support
-      export XDG_CURRENT_DESKTOP=sway
-
-      # Enable wayland backends
-      export XDG_SESSION_TYPE=wayland
-
-      # Use CLUTTER wayland backend
-      export CLUTTER_BACKEND=wayland
-
-      # Enable mozilla wayland backend
-      export MOZ_ENABLE_WAYLAND=1
-
-      # Enable smooth scrolling
-      export MOZ_USE_XINPUT2=1
-
-      # Enable mozilla dbus
-      export MOZ_DBUS_REMOTE=1
-
-      # Enable LibreOffice gtk3 backend
-      export SAL_USE_VCLPLUGIN=gtk3
-
-      # Configure Qt wayland backend
-      export QT_WAYLAND_FORCE_DPI=physical
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-
-      # Fix Java AWT applications
-      export _JAVA_AWT_WM_NONREPARENTING=1
-
-      # Use GTK theme and integration
-      export DESKTOP_SESSION=gnome
-      export QT_QPA_PLATFORMTHEME=gtk2
-
-      # Fix gsettings
-      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-    '';
 
     # Leave all other additional tools to user
     extraPackages = [];
