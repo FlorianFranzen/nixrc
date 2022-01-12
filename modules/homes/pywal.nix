b7784943267d4d4b75be841f901ea4df548e999c
Florian Franzen <Florian.Franzen@gmail.com>
2022-01-12 11:09:58 +0100

    pywal: theme sway with output

--- modules/homes/pywal.nix
+++ modules/homes/pywal.nix
@@ -14,6 +14,8 @@ let
   '';
 
   cache = mkWalCache cfg.theme;
+
+  result = builtins.fromJSON (builtins.readFile "${cache}/colors.json");
 in {
 
   options.pywal = {
@@ -32,5 +34,36 @@ in {
       # Load color scheme
       (cat ${cache}/sequences &)
     '';
+
+    wayland.windowManager.sway.config.colors = with result.colors;
+      let
+        default = {
+          background = color0;
+          text = color7;
+          indicator = color6;
+        };
+      in {
+        focused = default // {
+          border = color5;
+          childBorder = color5;
+        };
+        focusedInactive = default // {
+          border = color4;
+          childBorder = color4;
+        };
+        placeholder = default // {
+          border = color2;
+          childBorder = color2;
+        };
+        unfocused = default // {
+          border = color3;
+          childBorder = color3;
+        };
+        urgent = default // {
+          border = color8;
+          childBorder = color8;
+        };
+        background = color0;
+      };
   };
 }
