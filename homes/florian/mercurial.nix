a2749faada4df7da200a976a169aae50bf74fe45
Florian Franzen <Florian.Franzen@gmail.com>
2022-02-14 13:40:45 +0100

    florian: add mecurial config

---
+++ homes/florian/mercurial.nix
@@ -0,0 +1,24 @@
+{
+  programs.mercurial = rec {
+    enable = true;
+    userName = "Florian Franzen";
+    userEmail = "Florian@Franzen.io";
+    extraConfig = {
+      email = {
+        from = "${userName} <${userEmail}>";
+        charset = "utf-8";
+      };
+
+      extensions.patchbomb = "";
+
+      diff.git = 1;
+
+      smtp = {
+        host = "smtp.denkmuskel.org";
+        port = 587;
+        tls = "starttls";
+        username = "florian";
+      };
+    };
+  };
+}
