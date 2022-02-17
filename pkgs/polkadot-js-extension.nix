ce1300e41d0ba43b86f9301fd5d019de551fe383
Florian Franzen <Florian.Franzen@gmail.com>
2022-02-17 23:58:11 +0100

    polkadot-js: 0.40.2 -> 0.42.2

--- pkgs/polkadot-js-extension.nix
+++ pkgs/polkadot-js-extension.nix
@@ -2,10 +2,10 @@
 
 buildFirefoxXpiAddon {
   pname = "polkadot-js";
-  version = "0.40.2";
-  addonId = "{7e3ce1f0-15fb-4fb1-99c6-25774749ec6d}";
-  url = "https://addons.mozilla.org/firefox/downloads/file/3840870/polkadotjs_extension-0.40.2-fx.xpi";
-  sha256 = "fh/mq2hHgGAwZRaJExMkTfSDzMVTMl07IrtT2vfzytA=";
+  version = "0.42.2";
+  addonId = "{7e3cef0-15fb-4fb1-99c6-25774749ec6d}";
+  url = "https://addons.mozilla.org/firefox/downloads/file/3879725/polkadotjs_extension-0.42.2-fx.xpi";
+  sha256 = "SoGFDrHdgwWfNjjhXP6DV+NLPP7W4Ua4jV3lx0e9RKI=";
   meta = with lib; {
     homepage = "https://github.com/polkadot-js/extension";
     description = "Manage your Polkadot accounts outside of dapps. Injects the accounts and allows signs transactions for a specific account.";
