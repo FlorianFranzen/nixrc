47b53df4db1cf6c12ea6df40fe40957b2b6e7c59
Florian Franzen <Florian.Franzen@gmail.com>
2022-05-23 01:12:24 +0200

    unstable-overlay: update hydra and pin its nix too

--- pkgs/unstable.nix
+++ pkgs/unstable.nix
@@ -8,4 +8,10 @@ final: prev:
 
   # Packages to take from unstable
   inherit (channels.unstable) nix wofi zsa-udev-rules;
+
+  hydra-unstable = channels.unstable.hydra-unstable.override {
+    # Use same nix for hydra as well.
+    inherit (final) nix;
+  };
 }
+
