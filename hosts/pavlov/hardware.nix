d968f03f6345a01071bc1f5c7f07c62bf9547215
Florian Franzen <Florian.Franzen@gmail.com>
2022-05-22 17:29:15 +0200

    pavlov: cleanup btrfs config

--- hosts/pavlov/hardware.nix
+++ hosts/pavlov/hardware.nix
@@ -8,29 +8,39 @@
   boot.kernelModules = [ "kvm-intel" "it87" ];
   boot.extraModulePackages = [ ];
 
+  # Main SSD partition with various BTRFS subvolumes
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/7d69236c-bae0-4171-b06f-25f3b367510b";
       fsType = "btrfs";
-      options = [ "subvol=@nixos" ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/920B-CD82";
-      fsType = "vfat";
+      options = [ "subvol=@nixos" "compress=zstd" ];
     };
 
   fileSystems."/home" =
     { device = "/dev/disk/by-uuid/7d69236c-bae0-4171-b06f-25f3b367510b";
       fsType = "btrfs";
-      options = [ "subvol=@home" ];
+      options = [ "subvol=@home" "compress=zstd" ];
     };
 
+  fileSystems."/var/lib/jellyfin" =
+    { device = "/dev/disk/by-uuid/7d69236c-bae0-4171-b06f-25f3b367510b";
+      fsType = "btrfs";
+      options = [ "subvol=@jellyfin" "compress=zstd" ];
+    };
+
+  # EFI Boot Partition (ESP)
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/920B-CD82";
+      fsType = "vfat";
+    };
+
+  # TODO: Investigate if this makes any sense
   fileSystems."/tmp" =
     { device = "/dev/disk/by-uuid/7d69236c-bae0-4171-b06f-25f3b367510b";
       fsType = "btrfs";
       options = [ "subvol=@tmp" ];
     };
 
+  # Various ZFS media drives
   fileSystems."/media/addy" =
     { device = "data/media/addy";
       fsType = "zfs";
