b9254d4fd94b59d955951084c7e0a46c2a98bb1a
Florian Franzen <Florian.Franzen@gmail.com>
2022-12-08 18:02:35 +0100

    i3status: add spaces and icons to format

--- homes/florian/desktop/i3status.nix
+++ homes/florian/desktop/i3status.nix
@@ -17,11 +17,12 @@ in {
         blocks = [
           {
             block = "focused_window";
-            format = "{$title|}{ $visible_marks|}";
+            format = "{ $title |}{$visible_marks |}";
           }
           {
             block = "cpu";
             interval = 5;
+            format_alt = " $icon $frequency{ $boost|} ";
             click = [
               {
                 button = "left";
@@ -36,13 +37,13 @@ in {
           {
             block = "memory";
             interval = 5;
-            format = "$mem_used_percents";
-            format_alt = "$swap_used_percents";
+            format = " $icon $mem_used_percents ";
+            format_alt = " $icon $swap_used_percents ";
           }
           {
             block = "disk_space";
             path = "/";
-            format = "$available $percentage";
+            format = " $icon $available $percentage ";
             interval = 15;
             warning = 25.0;
             alert = 10.0;
@@ -50,12 +51,12 @@ in {
           {
             block = "battery";
             driver = "upower";
-            format = "$percentage $time";
+            format = " $icon $percentage $time ";
           }
           {
             block = "time";
             interval = 60;
-            format = "%a %d.%m.%Y %R";
+            format = " $icon %a %d.%m.%Y %R ";
           }
         ];
       };
@@ -64,13 +65,13 @@ in {
         blocks = [
           {
             block = "music";
-            format = "{$combo |}$prev $play $next";
+            format = " {$combo |}$prev $play $next ";
             hide_when_empty = true;
           }   
           {
             block = "sound";
             max_vol = 100;
-            format = "$output_description.rot-str(20){ $volume|}";
+            format = " $icon $output_description.rot-str(20){ $volume|} ";
             click = [{
               button = "left";
               cmd = "${pkgs.pavucontrol}/bin/pavucontrol --tab=3";
@@ -88,11 +89,10 @@ in {
           {
             block = "bluetooth";
             mac = "38:18:4C:D3:F5:A0";
-            format = "Headphones{ $percentage|}";
           }
           {
             block = "net";
-            format = "{$ssid ($signal_strength $frequency)|$device}";
+            format = " $icon {$ssid ($signal_strength $frequency)|$device} ";
             click = [{
               button = "left";
               cmd = "${terminal} ${pkgs.iwd}/bin/iwctl";
