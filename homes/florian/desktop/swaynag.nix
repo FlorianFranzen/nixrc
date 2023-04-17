6a59d4eff1fc22832b0f1de7f1e7d8681377997c
Florian Franzen <Florian.Franzen@gmail.com>
2023-04-17 17:02:25 +0200

    swaynag-battery: improve systemd service

--- homes/florian/desktop/swaynag.nix
+++ homes/florian/desktop/swaynag.nix
@@ -6,9 +6,13 @@
       Description = "Low battery notification";
       PartOf = "graphical-session.target";
     };
+
     Service = {
       Type = "simple";
       ExecStart = "${pkgs.swaynag-battery}/bin/swaynag-battery";
+      Restart = "on-failure";
     };
+
+    Install.WantedBy = [ "sway-session.target" ];
   };
 }
