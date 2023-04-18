0bf138c5d60dba64c99da9b460fa76da8f54bb8d
Florian Franzen <Florian.Franzen@gmail.com>
2023-04-18 12:17:06 +0200

    waydroid: retire custom derivation

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -58,8 +58,6 @@ in {
     tab-stash = super.callPackage ./tab-stash.nix {};
   };
 
-  waydroid = super.callPackage ./waydroid.nix {};
-
   # Update zeal slightly
   zeal = callOverride ./zeal.nix {};
 }
