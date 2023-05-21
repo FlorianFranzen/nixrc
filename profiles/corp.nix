3107912e3ea998f88932d878271fc366a7f2938f
Florian Franzen <Florian.Franzen@gmail.com>
2023-05-22 01:18:37 +0200

    corp: add obs-studio and virtual cam

--- profiles/corp.nix
+++ profiles/corp.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
 {
+  # Support virtual cameras
+  boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+
   environment.systemPackages = with pkgs; [
+    obs-studio
+
     slack
     zoom-us
   ];
+
 }
 
