f426de277b0921dbbc724cdf64204e1ed3345dec
Florian Franzen <Florian.Franzen@gmail.com>
2023-11-14 16:29:34 +0100

    gaming: update tooling

--- hosts/profiles/gaming.nix
+++ hosts/profiles/gaming.nix
@@ -1,31 +1,22 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Additional video accelaration
-  hardware = {
-    opengl = {
-      extraPackages = with pkgs; [
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-
-      extraPackages32 = with pkgs.pkgsi686Linux; [
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-    };
-  };
-
   # Enable 32bit pipewire alsa, if pipewire alsa is enabled
   services.pipewire.alsa.support32Bit = config.services.pipewire.alsa.enable;
 
   # Setup steam environment
   programs.steam.enable = true;
 
+  # Provide microphone noide filtering
+  programs.noisetorch.enable = true;
+
   # Additional games to install
   environment.systemPackages = with pkgs; [
     _20kly
+    cemu
     discord
+    goverlay
+    mangohud
     minecraft
     mindustry
     openjk
