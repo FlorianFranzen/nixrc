152d73a6cfeb2bb3f2732f913579c45228987386
Florian Franzen <Florian.Franzen@gmail.com>
2024-02-01 23:15:22 +0100

    fawkes: add case fancontrol for gpu

---
+++ hosts/configs/fawkes/fans.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+{
+  # Use lower case fan to provide more air to gpu
+  hardware.fancontrol = {
+    enable = true;
+
+    config = ''
+      INTERVAL=10
+      DEVPATH=hwmon0=devices/pci0000:00/0000:00:01.1/0000:01:00.0/0000:02:00.0/0000:03:00.0 hwmon4=devices/platform/nct6775.656
+      DEVNAME=hwmon0=amdgpu hwmon4=nct6799
+      FCTEMPS=hwmon4/pwm6=hwmon0/temp1_input
+      FCFANS=hwmon4/pwm6=hwmon4/fan6_input
+      MINTEMP=hwmon4/pwm6=40
+      MAXTEMP=hwmon4/pwm6=80
+      MINSTART=hwmon4/pwm6=32
+      MINSTOP=hwmon4/pwm6=24
+      MINPWM=hwmon4/pwm6=24
+      MAXPWM=hwmon4/pwm6=192
+    '';
+  };
+}
