4a1574094073bb1687659fbef183e8fcf389bbec
Florian Franzen <Florian.Franzen@gmail.com>
2024-02-21 20:50:24 +0100

    fawkes: enable wake-on-lan

--- hosts/configs/fawkes/networking.nix
+++ hosts/configs/fawkes/networking.nix
@@ -2,9 +2,14 @@
 
 {
   networking.interfaces= {
-    # Enable DHCP on all interfaces
+    # Enable DHCP and WakeOnLAN on wired interfaces
     eno1.useDHCP = true;
+    eno1.wakeOnLan.enable = true;
+
     eno2.useDHCP = true;
-    wlp7s0.useDHCP = true;
+    eno2.wakeOnLan.enable = true;
+
+    # Enable DHCP on wireless interfaces (hangs boot)
+    #wlp7s0.useDHCP = true;
   };
 }
