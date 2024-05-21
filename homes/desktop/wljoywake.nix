bae6763d2be0246d2ca8c8aa8ac026568a3fe9c6
Florian Franzen <Florian.Franzen@gmail.com>
2024-05-21 14:58:09 +0200

    systemd: fix idle inhibitor dependencies

--- homes/desktop/wljoywake.nix
+++ homes/desktop/wljoywake.nix
@@ -4,14 +4,15 @@
   systemd.user.services.wljoywake = {
     Unit = {
       Description = "Wayland idle inhibit on joystick input";
-      After = [ "graphical-session-pre.target" "swayidle.service" ];
-      Requires = [ "swayidle.service" ];
-      BindsTo = [ "swayidle.service" ];
+      After = [ "swayidle.service" ];
       PartOf = [ "graphical-session.target" ];
     };
+
     Service = {
       ExecStart = "${pkgs.wljoywake}/bin/wljoywake";
       Restart = "always";
     };
+
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
--- homes/desktop/wscreensaver-bridge.nix
+++ homes/desktop/wscreensaver-bridge.nix
@@ -4,15 +4,16 @@
   systemd.user.services.wscreensaver-bridge = {
     Unit = {
       Description = "Wayland Screensaver Bridge";
-      After = [ "graphical-session-pre.target" "swayidle.service" ];
-      Requires = [ "swayidle.service" ];
-      BindsTo = [ "swayidle.service" ];
+      After = [ "swayidle.service" ];
       PartOf = [ "graphical-session.target" ];
     };
+
     Service = {
       Type = "dbus";
       BusName = "org.freedesktop.ScreenSaver";
       ExecStart = "${pkgs.wscreensaver-bridge}/bin/wscreensaver-bridge";
     };
+
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
