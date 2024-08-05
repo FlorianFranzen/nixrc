968899e9e44d40038835c58d29f14966838e76a7
Florian Franzen <Florian.Franzen@gmail.com>
2024-08-05 11:12:51 +0200

    unfree: add missing packages

--- hosts/modules/unfree.nix
+++ hosts/modules/unfree.nix
@@ -7,12 +7,16 @@
     "nvidia-x11" "nvidia-persistenced" "nvidia-settings" 
     # Unfree MacBook wireless
     "broadcom-sta"
+    # Unfree wooting utility
+    "wootility"
     # Nitrokey firmware updates
     "nrfutil" "pc-ble-driver-py" "pc-ble-driver" "pypemicro"
     # Steam and other gaming
     "steam" "steam-original" "steam-run" "minecraft-launcher" "clonehero" "discord"
     # Printer and scanner drivers
     "cups-brother-mfcl2710dw" "brscan4" "brother-udev-rule-type1" "brscan4-etc-files"
+    # Dictionaries
+    "aspell-dict-en-science"
     # Some corpoware
     "slack" "teams-on-linux" "zoom"
   ];
