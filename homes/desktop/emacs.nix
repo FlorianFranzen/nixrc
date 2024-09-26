a3e68d606811dbce562765a83ecf502cb51d98dd
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-27 01:20:09 +0200

    emacs: stop using buggy server

--- homes/desktop/emacs.nix
+++ homes/desktop/emacs.nix
@@ -3,8 +3,5 @@
 {
   # Special wayland friendly version
   programs.emacs.package = pkgs.emacs29-pgtk;
-
-  # Use same package for server
-  services.emacs.package = pkgs.emacs29-pgtk;
 }
 
--- homes/terminal/emacs.nix
+++ homes/terminal/emacs.nix
@@ -16,26 +16,4 @@
     # Do not include any ui toolkit by default
     package = lib.mkDefault pkgs.emacs29-nox;
   };
-
-  # Setup emacs ...
-  services.emacs = {
-    # ... via local emacs server ...
-    enable = true;
-
-    # ... to be used by default
-    client.enable = true;
-    defaultEditor = true;
-
-    # Use same ui-less package for server by default
-    package = lib.mkDefault pkgs.emacs29-nox;
-
-    # Only autostart in graphical session ...
-    startWithUserSession = "graphical";
-
-    # ... otherwise socket activate
-    socketActivation.enable = true;
-  };
-
-  # Provide server with access to user ssh auth socket
-  systemd.user.services.emacs.Service.Environment = "SSH_AUTH_SOCK=/run/user/%U/gnupg/S.gpg-agent.ssh";
 }
