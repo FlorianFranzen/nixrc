4a3809021abb34af76cd9348ac9da0c90b55b369
Florian Franzen <Florian.Franzen@gmail.com>
2024-11-30 21:38:33 +0100

    kde: use ozone and add partion-manager

--- nixos/profiles/desktops/kde.nix
+++ nixos/profiles/desktops/kde.nix
@@ -1,11 +1,20 @@
 { pkgs, ... }:
 
 {
+  # Use ozone wayland backedn
+  environment.variables.NIXOS_OZONE_WL = 1;
+
+  # Add additional image formats
   environment.systemPackages = [
     pkgs.kdePackages.qtimageformats
   ];
 
+  # Enable connect support
   programs.kdeconnect.enable = true;
 
+  # Enable partition management
+  programs.partition-manager.enable = true;
+
+  # Enable lastest plasma release
   services.desktopManager.plasma6.enable = true;
 }
