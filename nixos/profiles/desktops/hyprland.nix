289367377dc6ad6162e5bf6225f7a1e2f629ab13
Florian Franzen <Florian.Franzen@gmail.com>
2024-12-19 19:15:52 +0100

    hyperland: improve qt and wayland integration

--- nixos/profiles/desktops/hyprland.nix
+++ nixos/profiles/desktops/hyprland.nix
@@ -7,6 +7,17 @@
   # Install some base necessities
   hardware.graphics.enable = true;
 
+  environment.systemPackages = with pkgs; [
+    qt5.qtwayland
+    qt6.qtwayland
+    libsForQt5.qtstyleplugins
+    qt6gtk2
+    vulkan-validation-layers
+    wl-clipboard
+  ];
+
+  hardware.opengl.enable = true;
+
   fonts.enableDefaultPackages = true;
 
   programs.dconf.enable = true;
