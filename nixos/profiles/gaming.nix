f711c183208ca017a31a3663a919756fd86bbb89
Florian Franzen <Florian.Franzen@gmail.com>
2025-04-20 02:24:47 +0200

    gaming: enable gamescope and its session

--- nixos/profiles/gaming.nix
+++ nixos/profiles/gaming.nix
@@ -4,8 +4,19 @@
   # Enable 32bit pipewire alsa, if pipewire alsa is enabled
   services.pipewire.alsa.support32Bit = config.services.pipewire.alsa.enable;
 
+  # Setup gamescope helper
+  programs.gamescope = {
+    enable = true;
+
+    capSysNice = true;
+  };
+
   # Setup steam environment
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+
+    gamescopeSession.enable = true;
+  };
 
   # Provide microphone noide filtering
   programs.noisetorch.enable = true;
