a6d914fa5fb73fbb420852045984988cbd1abab5
Florian Franzen <Florian.Franzen@gmail.com>
2025-08-28 00:36:59 +0200

    emacs: extend org mode capabilities

--- emacs/init.el
+++ emacs/init.el
@@ -151,7 +151,7 @@
        ;;nim               ; python + lisp at the speed of c
        (nix +lsp +tree-sitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +dragndrop +pandoc +present +pretty) ; organize your plain life in plain text
+       (org +dragndrop +journal +noter +pandoc +present +pretty) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
