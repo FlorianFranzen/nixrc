1f17e91d9bfe8bcaec27ecb1436ce504e0be61cf
Florian Franzen <Florian.Franzen@gmail.com>
2026-03-14 01:23:16 +0100

    patterson: complete network config

--- hosts/patterson/fstab.nix
+++ hosts/patterson/fstab.nix
@@ -2,17 +2,19 @@
 
 {
   # Root filesystem, boot and swap on NVME
-  fileSystems."/" = { 
-    device = "/dev/disk/by-uuid/61032b39-177e-48d6-b708-6f4f01b598d0";
-    fsType = "ext4";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/61032b39-177e-48d6-b708-6f4f01b598d0";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/3599-BE57";
+      fsType = "vfat";
+    };
   };
 
-  fileSystems."/boot" = { 
-    device = "/dev/disk/by-uuid/3599-BE57";
-    fsType = "vfat";
-  };
-
-  swapDevices = [ 
+  swapDevices = [
     { device = "/dev/disk/by-uuid/3c142f56-6f1a-4a75-b68c-4e0230606621"; }
   ];
 }
--- hosts/patterson/network.nix
+++ hosts/patterson/network.nix
@@ -1,16 +1,26 @@
 { ... }:
 
 {
-  # Configure main ethernet interface
-  networking.interfaces.end0 = {
-    # - default static ip address
-    ipv4.addresses = [{
-      address = "10.64.2.5";
-      prefixLength = 24;
-    }];
-  
-    # - enable DHCP as well
-    useDHCP = true;
+  networking = {
+    # Configure default gateway
+    defaultGateway = {
+      address = "10.64.2.254";
+      interface = "end0";
+    };
+
+    # Configure main ethernet interface
+    interfaces.end0 = {
+      ipv4.addresses = [{
+        address = "10.64.2.5";
+        prefixLength = 24;
+      }];
+    };
+
+    # Configure default nameserver
+    nameservers = [ "10.64.2.254" ];
+
+    # Disable default DHCP
+    useDHCP = false;
   };
 }
 
