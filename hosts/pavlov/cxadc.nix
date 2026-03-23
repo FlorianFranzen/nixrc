e7a1ea8ec159b3ae9184749f962ee9ebdeee4e6b
Florian Franzen <Florian.Franzen@gmail.com>
2026-03-23 23:44:08 +0100

    cxadc: prepare clockgen mod

--- hosts/pavlov/cxadc.nix
+++ hosts/pavlov/cxadc.nix
@@ -10,7 +10,7 @@ in {
     # Ensure kernel module is loaded
     kernelModules = [ "cxadc" ];
   };
-  
+
   # Load included modprobe config
   environment.etc."modprobe.d/cxadc.conf".source = "${kernelPkgs.cxadc}/lib/modprobe.d/cxadc.conf";
 
@@ -20,9 +20,31 @@ in {
     pkgs.flac
   ];
 
-  # Load included udev config
-  services.udev.packages = [ kernelPkgs.cxadc ];
+  # Enable alsa integration for clockgen mod
+  hardware.alsa.enable = true;
+
+  services.udev = {
+    # Load included udev config
+    packages = [ kernelPkgs.cxadc ];
+
+    # Extend udev config for clockgen mod
+    extraRules = ''
+      # Initialize cxadc0 with correct vmux, crystal, center, level and gain
+      KERNEL=="cxadc0", SUBSYSTEM=="cxadc", ATTR{device/parameters/vmux}="1"
+      KERNEL=="cxadc0", SUBSYSTEM=="cxadc", ATTR{device/parameters/crystal}="40000000"
+      KERNEL=="cxadc0", SUBSYSTEM=="cxadc", ATTR{device/parameters/center_offset}="1"
+      KERNEL=="cxadc0", SUBSYSTEM=="cxadc", ATTR{device/parameters/level}="0"
+      KERNEL=="cxadc0", SUBSYSTEM=="cxadc", ATTR{device/parameters/sixdb}="0"
+
+      # Initialize cxadc1 with correct vmux, crystal, center, level and gain
+      KERNEL=="cxadc1", SUBSYSTEM=="cxadc", ATTR{device/parameters/vmux}="1"
+      KERNEL=="cxadc1", SUBSYSTEM=="cxadc", ATTR{device/parameters/crystal}="40000000"
+      KERNEL=="cxadc1", SUBSYSTEM=="cxadc", ATTR{device/parameters/center_offset}="1"
+      KERNEL=="cxadc1", SUBSYSTEM=="cxadc", ATTR{device/parameters/level}="0"
+      KERNEL=="cxadc1", SUBSYSTEM=="cxadc", ATTR{device/parameters/sixdb}="0"
+    '';
+  };
 
   # Allow rootless access for default user
-  users.extraUsers.${username}.extraGroups = [ "video" ];
+  users.extraUsers.${username}.extraGroups = [ "audio" "video" ];
 }
