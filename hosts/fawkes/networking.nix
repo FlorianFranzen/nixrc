c2b7bfb9bb269577c892aa8fa5d0a88e8786ea62
Florian Franzen <Florian.Franzen@gmail.com>
2026-03-25 14:24:57 +0100

    fawkes: use static ip

--- hosts/fawkes/networking.nix
+++ hosts/fawkes/networking.nix
@@ -1,15 +1,54 @@
 { ... }:
 
 {
-  networking.interfaces= {
-    # Enable DHCP and WakeOnLAN on wired interfaces
-    eno1.useDHCP = true;
-    eno1.wakeOnLan.enable = true;
+  networking = {
+    # Configure static gateway on primary interface
+    defaultGateway = {
+      address = "10.64.0.254";
+      interface = "eno2";
+    };
 
-    eno2.useDHCP = true;
-    eno2.wakeOnLan.enable = true;
+    defaultGateway6 = {
+      address = "fd64::fffe";
+      interface = "eno2";
+    };
 
-    # Enable DHCP on wireless interfaces (hangs boot)
-    #wlp7s0.useDHCP = true;
+    interfaces = {
+      # Configure primary ethernet interface
+      eno2 = {
+        ipv4.addresses = [{
+          address = "10.64.0.10";
+          prefixLength = 24;
+        }];
+
+        ipv6.addresses = [{
+          address = "fd64::a";
+          prefixLength = 64;
+        }];
+
+        wakeOnLan.enable = true;
+      };
+
+      # Configure secondary ethernet interface
+      eno1 = {
+        ipv4.addresses = [{
+          address = "10.64.0.11";
+          prefixLength = 24;
+        }];
+
+        ipv6.addresses = [{
+          address = "fd64::b";
+          prefixLength = 64;
+        }];
+
+        wakeOnLan.enable = true;
+      };
+
+      # Enable DHCP on wireless interfaces
+      wlan0.useDHCP = true;
+    };
+
+    # Configure static nameservers
+    nameservers = [ "10.64.0.254" "fd64::fffe" ];
   };
 }
