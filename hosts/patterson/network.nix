04f927a3d8f541bbd1da627081ec75c00d2a578f
Florian Franzen <Florian.Franzen@gmail.com>
2026-03-25 14:35:24 +0100

    patterson: add static ipv6 address

--- hosts/patterson/network.nix
+++ hosts/patterson/network.nix
@@ -8,19 +8,28 @@
       interface = "end0";
     };
 
+    defaultGateway6 = {
+      address = "fd64:0:0:2::fffe";
+      interface = "end0";
+    };
+
     # Configure main ethernet interface
     interfaces.end0 = {
       ipv4.addresses = [{
         address = "10.64.2.5";
         prefixLength = 24;
       }];
+
+      ipv6.addresses = [{
+        address = "fd64:0:0:2::5";
+        prefixLength = 64;
+      }];
     };
 
     # Configure default nameserver
-    nameservers = [ "10.64.2.254" ];
+    nameservers = [ "10.64.2.254" "fd64:0:0:2::fffe" ];
 
     # Disable default DHCP
     useDHCP = false;
   };
 }
-
