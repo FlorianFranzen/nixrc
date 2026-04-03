3533ffb9eac47eafb3faa9c84d1a515f0f491852
Florian Franzen <Florian.Franzen@gmail.com>
2026-04-03 04:47:03 +0200

    fawkes: switch to systemd networkd

--- hosts/fawkes/networking.nix
+++ hosts/fawkes/networking.nix
@@ -1,54 +1,41 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  systemd.network = {
+    # Use systemd networkd
+    enable = true;
+
+    networks = {
+      # Main 10G interface
+      "10-eno2" = {
+        matchConfig.Name = "eno2";
+        address = [ "10.64.0.10/24" "fd64::a/64" ];
+        routes = [
+          { Gateway = "10.64.0.254"; Metric = 10; }
+          { Gateway = "fd64::fffe"; Metric = 10; }
+	];
+        dns = [ "10.64.0.254" "fd64::fffe" ];
+      };
+      # Main 2.5G interface
+      "20-eno1" = {
+        matchConfig.Name = "eno1";
+        address = [ "10.64.0.11/24" "fd64::b/64" ];
+        routes = [
+          { Gateway = "10.64.0.254"; Metric = 20; }
+          { Gateway = "fd64::fffe"; Metric = 20; }
+        ];
+        dns = [ "10.64.0.254" "fd64::fffe" ];
+      };
+    };
+  };
+
   networking = {
-    # Configure static gateway on primary interface
-    defaultGateway = {
-      address = "10.64.0.254";
-      interface = "eno2";
-    };
+    # Disable generic receive offload
+    localCommands = ''
+      ${pkgs.ethtool}/bin/ethtool -K eno2 gro off
+    '';
 
-    defaultGateway6 = {
-      address = "fd64::fffe";
-      interface = "eno2";
-    };
-
-    interfaces = {
-      # Configure primary ethernet interface
-      eno2 = {
-        ipv4.addresses = [{
-          address = "10.64.0.10";
-          prefixLength = 24;
-        }];
-
-        ipv6.addresses = [{
-          address = "fd64::a";
-          prefixLength = 64;
-        }];
-
-        wakeOnLan.enable = true;
-      };
-
-      # Configure secondary ethernet interface
-      eno1 = {
-        ipv4.addresses = [{
-          address = "10.64.0.11";
-          prefixLength = 24;
-        }];
-
-        ipv6.addresses = [{
-          address = "fd64::b";
-          prefixLength = 64;
-        }];
-
-        wakeOnLan.enable = true;
-      };
-
-      # Enable DHCP on wireless interfaces
-      wlan0.useDHCP = true;
-    };
-
-    # Configure static nameservers
-    nameservers = [ "10.64.0.254" "fd64::fffe" ];
+    # Disable default DHCP client
+    useDHCP = false;
   };
 }
