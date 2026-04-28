7125793035c0bfb01194326bdf62722d4996ef67
Florian Franzen <Florian.Franzen@gmail.com>
2026-04-28 17:14:52 +0200

    fawkes: fix fan control on new kernel

--- hosts/fawkes/fans.nix
+++ hosts/fawkes/fans.nix
@@ -7,9 +7,9 @@
 
     config = ''
       INTERVAL=10
-      DEVPATH=hwmon0=devices/pci0000:00/0000:00:01.1/0000:01:00.0/0000:02:00.0/0000:03:00.0 hwmon7=devices/platform/nct6775.656 hwmon8=devices/pci0000:00/0000:00:18.3
-      DEVNAME=hwmon0=amdgpu hwmon7=nct6799 hwmon8=k10temp
-      FCTEMPS=hwmon7/pwm5=hwmon8/temp1_input hwmon7/pwm6=hwmon0/temp1_input
+      DEVPATH=hwmon4=devices/pci0000:00/0000:00:01.1/0000:01:00.0/0000:02:00.0/0000:03:00.0 hwmon7=devices/platform/nct6775.656 hwmon8=devices/pci0000:00/0000:00:18.3
+      DEVNAME=hwmon4=amdgpu hwmon7=nct6799 hwmon8=k10temp
+      FCTEMPS=hwmon7/pwm5=hwmon8/temp1_input hwmon7/pwm6=hwmon4/temp1_input
       FCFANS=hwmon7/pwm5=hwmon7/fan5_input hwmon7/pwm6=hwmon7/fan6_input
       MINTEMP=hwmon7/pwm5=50 hwmon7/pwm6=40
       MAXTEMP=hwmon7/pwm5=90 hwmon7/pwm6=80
